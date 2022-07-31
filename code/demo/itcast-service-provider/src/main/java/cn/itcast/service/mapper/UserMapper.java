c9c8e422c89b34b67af69b64b9892a0d772c16da
炸薯片 <876763275@qq.com>
2022-07-31 22:44:13 +0800

    初始化仓库

--- code/demo/itcast-service-provider/src/main/java/cn/itcast/service/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.service.mapper;
-
-import cn.itcast.service.pojo.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
--- code/springboot/springcloud_3_provider/src/main/java/com/springcloud_3_provider/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.springcloud_3_provider.mapper;
-
-
-import com.springcloud_3_provider.pojo.User;
-import tk.mybatis.mapper.common.Mapper;
-
-
-public interface UserMapper extends Mapper<User> {
-}
